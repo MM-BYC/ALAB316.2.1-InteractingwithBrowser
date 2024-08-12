0d7a9da5136ea68394d6298e58c426dc59c5a3d3
mmarque <marquezmusa@gmail.com>
2024-08-12 17:57:35 -0400

    Change again donaldduck

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 successfulMatchCount = 0;
 const owl = "./images/owl.jpg";
 const boy = "./images/astronaut.jpg";
-const duck = "./images/donaldDuck.jpeg";
+const duck = "./images/duck.jpeg";
 const dog = "./images/dog.jpeg";
 const letters = [boy, boy, owl, owl, duck, duck, dog, dog];
 
